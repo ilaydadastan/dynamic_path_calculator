1f82f949db96398f660b5c0fe612e35474727385
ilaydadastan <ilaydastan2@gmail.com>
2022-08-11 22:56:43 +0300

    added sum calculator controller

---
+++ README.md
@@ -0,0 +1,8 @@
+This is **Spring Boot** Project.
+I created SumController class in controller package.
+
+SumController class has HTTP GET Method to calculate sum of two int numbers.
+
+It used **Query String Parameter**.
+
+Example URL: http://localhost:8080/?n1=9&n2=3
---
+++ src/main/java/com/example/springboot/controller/SumController.java
@@ -0,0 +1,20 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SumController {
+
+    @GetMapping
+    public int hello(@RequestParam("n1") int n1, @RequestParam("n2") int n2){
+        System.out.println(n1);
+        System.out.println(n2);
+
+        return n1+n2;
+    }
+
+}
+
+
